44ad51091ebdf98cfec4c503da90be0c99e6d762
Ben Scott <ben@belfryimages.com.au>
2012-03-29 17:10:03 +1000

    success/failure pages, should redirect but doesn't ...

--- php/one_true_form_mailer/onetrueformmailer.php
+++ php/one_true_form_mailer/onetrueformmailer.php
@@ -6,16 +6,35 @@
 
 // user configuration
 	// from, to, subject, redirect
+$redirectSuccess = 'success.html';
+$redirectFailure = 'failure.html';
+
 // get form values
 
 // build message to mail, use ob_XXX() methods to generate HTML message
 
+
+
+
+
+
 // ----- NOTHING BELOW THIS LINE NEEDS TO BE CHANGED -----
+
+var $otfm = new OneTrueFormMailer();
 
 // mail it
 
 // do redirection
+$otfm->redirectTo($redirectSuccess);
 
 // methods hidden down below all the action...
+class OneTrueFormMailer {
+	function redirectTo($url) { 
+		header('Status: 302');
+		header("Location: {$url}");
+		die();
+	}
+}
+
 
 ?>
