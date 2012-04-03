39e1876736b1d792bd8f8f6a285c76920fb63fd4
Ben Scott <ben@belfryimages.com.au>
2012-04-03 14:52:07 +1000

    attachments, updated readme

--- php/one_true_form_mailer/onetrueformmailer.php
+++ php/one_true_form_mailer/onetrueformmailer.php
@@ -1,5 +1,6 @@
 <?php
 // The one true form mailer
+// CIPL IT Services - ciplit.com.au
 // CC BY-SA 3.0
 // See https://github.com/ciplit/Shu-Er
 
@@ -15,6 +16,11 @@ $otfm = new OneTrueFormMailer(array(
 
 // remove this line if empty forms are OK
 if (empty($_REQUEST)) $otfm->failure();
+
+// Add attachments (modify or remove as required):
+$otfm->addAttachment('attachment1');
+$otfm->addAttachment('attachment2');
+$otfm->addAttachment('attachment3');
 
 // Build up your HTML email:
 ?>
@@ -33,29 +39,16 @@ if (empty($_REQUEST)) $otfm->failure();
 </html>
 <?php
 
-// This example just dumps all fields:
-/*
-?>
-<html>
-	<body>
-		<dl>
-			<?php foreach ($_REQUEST as $k => $v) { ?>
-				<dt><?php eh($k); ?></dt>
-				<dd><?php ep($v); ?></dd>
-			<?php } ?>
-		</dl>
-	</body>
-</html>
-<?php
-*/
 
+
+
+
+// -------------------------------------------------------
 // ----- NOTHING BELOW THIS LINE NEEDS TO BE CHANGED -----
-
-
+// -------------------------------------------------------
 
 $otfm->sendIt();
 
-function form($f) { eh($_REQUEST[$f]); }
 function e($s) { echo($s); }
 function h($s) { return htmlentities($s); }
 function eh($s) { e(h($s)); }
@@ -69,11 +62,26 @@ function str_contains($source, $search) {
 
 class OneTrueFormMailer {
 	var $settings = null;
+	var $attachments = array();
 
 	function OneTrueFormMailer($settings) {
 		$this->settings = $settings;
 		ob_clean();
 		ob_start();
+	}
+
+	function addAttachment($k) {
+		if (!isset($_FILES[$k])) return;
+		
+		$a = $_FILES[$k];
+		
+		if (!isset($a['error'])) return;
+		if ($a['error'] == 4) return;
+
+		$this->attachments[$a['name']] = array(
+			'type' => $a['type'],
+			'data' =>$this->__readPostedFile($a['tmp_name'])
+		);
 	}
 
 	function sendIt() {
@@ -105,13 +113,27 @@ class OneTrueFormMailer {
 			$message.PHP_EOL.
 			PHP_EOL;
 
+		foreach ($this->attachments as $k => $v) {
+			$attachment = chunk_split(base64_encode($v['data']));
+			$content .=
+				"--{$boundary}".PHP_EOL.
+				"Content-type: application/octet-stream; name=\"{$k}\"".PHP_EOL.
+				"Content-Transfer-Encoding: base64".PHP_EOL.
+				"Content-Disposition: attachment; filename=\"{$k}\"".PHP_EOL.
+				PHP_EOL.
+				$attachment.PHP_EOL.
+				PHP_EOL;				
+		}
+
+		$content .= "--{$boundary}--";
+
 		$result = mail(
 			$to,
 			$subject,
 			$content,
 			$headers
 			);
-		if (!$result) die();// return $this->failure();
+		if (!$result) return $this->failure();
 		return $this->success();
 	}
 
@@ -122,6 +144,7 @@ class OneTrueFormMailer {
 		header("Location: {$url}");
 		die();
 	}
+
 	function __checkForHeaderInjection($inputs) {
 		$injectionPrej = 
 			'/\b^to+(?=:)\b|^content-type:|^cc:|^bcc:|^from:|^subject:|^mime-version:|^content-transfer-encoding:/im';
@@ -129,9 +152,28 @@ class OneTrueFormMailer {
 			if (preg_match($injectionPrej, $s)) $this->failure();
 		}
 	}
+
 	function __checkReferrer() {
 		if (empty($_SERVER['HTTP_REFERER'])) $this->failure();
 		if (!str_contains($_SERVER['HTTP_REFERER'], $_SERVER['HTTP_HOST'])) $this->failure();
+	}
+
+	function __readPostedFile($f) {
+		if (!is_uploaded_file($f)) throw new Exception('Invalid uploaded file location');		
+		if (!file_exists($f)) throw new Exception('File does not exist');
+		
+		$handle = @fopen($f, 'rb');
+		if ($handle === FALSE) throw new Exception('File could not be opened. Please try again.');
+		
+		$size = filesize($f);
+		if ($size == 0) throw new Exception('File has zero length. Please try again.');
+		
+		$data = @fread($handle, $size);
+		if ($data === FALSE) throw new Exception('File could not be read. Please try again.');
+		
+		fclose($handle);
+		
+		return $data;
 	}
 }
 
--- php/one_true_form_mailer/readme.md
+++ php/one_true_form_mailer/readme.md
@@ -4,8 +4,50 @@ One form mailer to rule them all.
 
 
 ## Usage
-You only need `onetrueformmailer.php`, the rest of the files are for testing the script. All of the configuration is done at the top of `onetrueformmailer.php`. Replace `success.html` and `failure.html` with your own, or configure them to different paths in the script.
+You only need `onetrueformmailer.php`, the rest of the files are for testing the script.
+
+
+### Configuration
+
+All of the configuration is done at the top of `onetrueformmailer.php`. Replace `success.html` and `failure.html` with your own, or configure them to different paths in the script.
+
+Fields from the request could be used in the configuration, for example the 'from' address could be set to `$_REQUEST['email']`. **This is not recommended** as it increases the risk of header injection, although checks are done before sending the email (in `sendIt()`).
+
+
+### Email template
 
 Build up the HTML email, using `$_REQUEST` to pull out the form values. Remember to use `eh()` or `ep()` to print the values, otherwise you could expose yourself to email header injection attacks.
 
-There is an example which dumps out all fields in the submitted request, which could be useful if you don't want to maintain the script with the form, or don't neccessarily know what fields are coming in. Be aware that this may break if there are attachments.
+Some helper methods exist for printing fields within the email template:
+
+- `eh($s)` - converts to HTML entities (to reduce injection attacks) then prints
+- `ep($s)` - converts to HTML entities, then replaces new line characters with line breaks, then prints. Useful for printing `textarea` fields.
+- `pr($o)` - pretty prints an object wrapped in `<pre>` tags. Useful for debugging (`<?php pr($_REQUEST); ?> dumps the entire request)
+
+
+This example template just dumps out all fields in the submitted request, which could be useful if you don't want to maintain the script with the form, or don't neccessarily know what fields are coming in.
+
+	<html>
+		<body>
+			<dl>
+				<?php foreach ($_REQUEST as $k => $v) { ?>
+					<dt><?php eh($k); ?></dt>
+					<dd><?php ep($v); ?></dd>
+				<?php } ?>
+			</dl>
+		</body>
+	</html>
+
+
+
+### Attachments
+
+Attachments are added one by one by calling:
+
+	$otfm->addAttachment('form_input_name');
+
+If a file isn't selected, `addAttachment()` will silently skip it, so validation needs to be done client-side or manually.
+
+To add all attachments, do the following (but be aware that this would allow an attacker to abuse the attachments):
+
+	foreach ($_FILES as $k => $v) $otfm->addAttachment($k);
