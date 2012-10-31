1564b12c8c34e13cc6fc50227a5e65356687d425
Ben Scott <ben@belfryimages.com.au>
2012-10-31 11:08:46 +1000

    Built-in insert and update methods

--- dotnet/DataAccess/src/Swxben.DataAccess/DataAccess.cs
+++ dotnet/DataAccess/src/Swxben.DataAccess/DataAccess.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Dynamic;
 using System.Linq;
+using System.Text;
 
 namespace SwxBen
 {
@@ -11,6 +12,9 @@ namespace SwxBen
         int ExecuteCommand(string sql, object parameters = null);
         IEnumerable<dynamic> ExecuteQuery(string sql, object parameters = null);
         IEnumerable<T> ExecuteQuery<T>(string sql, object parameters = null) where T : new();
+        void Insert<T>(T value);
+        void Update<T>(T value, string id);
+        void DropTable(string tableName);
     }
 
     public class DataAccess : IDataAccess
@@ -132,6 +136,72 @@ namespace SwxBen
             }
 
             command.Parameters.Add(parameter);
+        }
+
+        public void Insert<T>(T value)
+        {
+            ExecuteCommand(GetInsertSqlFor<T>(), value);
+        }
+
+        public void Update<T>(T value, string id)
+        {
+            ExecuteCommand(GetUpdateSqlFor<T>(id), value);
+        }
+
+        public static string GetInsertSqlFor<T>()
+        {
+            var fieldsSql = new StringBuilder();
+            var valuesSql = new StringBuilder();
+            foreach (var field in GetAllFieldNames<T>())
+            {
+                if (!string.IsNullOrEmpty(fieldsSql.ToString()))
+                {
+                    fieldsSql.Append(", ");
+                    valuesSql.Append(", ");
+                }
+                fieldsSql.Append(field);
+                valuesSql.Append("@").Append(field);
+            }
+
+            return string.Format(
+                "INSERT INTO {0}s({1}) VALUES({2})",
+                typeof(T).Name,
+                fieldsSql,
+                valuesSql);
+        }
+
+        public static string GetUpdateSqlFor<T>(string id)
+        {
+            var set = new StringBuilder();
+
+            foreach (var field in GetAllFieldNames<T>())
+            {
+                if (field.ToUpper() == id.ToUpper()) continue;
+                if (!string.IsNullOrEmpty(set.ToString())) set.Append(", ");
+                set.AppendFormat("{0} = @{0}", field);
+            }
+
+            return string.Format(
+                "UPDATE {0}s SET {1} WHERE {2} = @{2}",
+                typeof(T).Name,
+                set,
+                id);
+        }
+
+        private static IEnumerable<string> GetAllFieldNames<T>()
+        {
+            return
+                typeof(T).GetFields().Select(f => f.Name)
+                .Concat(typeof(T).GetProperties().Select(p => p.Name));
+        }
+
+        public void DropTable(string tableName)
+        {
+            var sql = string.Format(@"
+IF EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[{0}]') AND type IN (N'U'))
+    DROP TABLE {0}", tableName);
+
+            ExecuteCommand(sql);
         }
     }
 }
