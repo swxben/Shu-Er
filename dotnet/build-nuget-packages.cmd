d47b52bea493c80a1fff6f7cd5cfd44a95d6a7e0
Ben Scott <ben@belfryimages.com.au>
2013-06-13 07:05:51 +1000

    New empty 2012 solution. Update nuget template.

--- dotnet/build-nuget-packages.cmd
+++ dotnet/build-nuget-packages.cmd
@@ -3,9 +3,19 @@ echo *** Make sure you have updated the assembly and nuspec to match the release
 pause
 
 call "%VS100COMNTOOLS%vsvars32.bat"
-mkdir log\
-mkdir lib\net40\
+mkdir log
+mkdir lib\net40
+mkdir nupkg_archive
 
 msbuild.exe /ToolsVersion:4.0 "src\project\project.csproj" /p:configuration=Release
-utilities\nuget.exe pack project.nuspec
+.nuget\nuget.exe pack project.nuspec
+
+echo *** Ready to upload to nuget.org ***
 pause
+
+for %%f in (*.nupkg) do (
+	.nuget\nuget.exe push %%f
+)
+
+copy *.nupkg nupkg_archive\
+del *.nupkg
